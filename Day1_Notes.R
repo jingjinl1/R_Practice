19eb5add2ce4f5c038996e0095b201648ee0c912
Jingjin Li <JJL@townsvilleport.com.au>
2025-01-28 14:57:09 +1000

    day 1 notes

---
+++ Day1_Notes.R
@@ -0,0 +1,13 @@
+#Notes - Day1
+#Created: 28/01/2025
+#Created by: JJ
+
+#Basic commands#############################################
+# Clear packages
+detach("package:datasets", unload = TRUE)  # For base
+
+# Clear plots
+graphics.off()  # Clears plots, closes all graphics devices
+
+# Clear console
+cat("\014")  # ctrl+L
